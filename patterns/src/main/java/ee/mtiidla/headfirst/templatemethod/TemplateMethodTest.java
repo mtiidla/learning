ffa910aa6bbf1ad91bf0470446d8522eb88913c2
Marko Tiidla <marko.tiidla@tattoodo.com>
2018-04-23 21:54:36 +0200

    Add template method definition

--- patterns/src/main/java/ee/mtiidla/headfirst/templatemethod/TemplateMethodTest.java
+++ patterns/src/main/java/ee/mtiidla/headfirst/templatemethod/TemplateMethodTest.java
@@ -1,5 +1,10 @@
 package ee.mtiidla.headfirst.templatemethod;
 
+/**
+ * The Template Method Pattern defines the skeleton of an algorithm in a method, deferring some
+ * steps to subclasses. Template Method lets subclasses redefine certain steps of an algorithm
+ * without changing the algorithm's structure.
+ */
 class TemplateMethodTest {
 
     public static void main(String[] args) {
